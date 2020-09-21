60f23e70e57e1b9c77ad8d42bc54c878f5525f21
Maicon Corceski <maicon.corceski@gmail.com>
2020-09-21 19:01:47 -0300

    added panel component

--- dashboard/src/components/Panel/index.tsx
+++ dashboard/src/components/Panel/index.tsx
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { PanelWrapper } from './styles';
 import { PanelProps } from '../../types/site';
+import MessageBox from '../MessageBox';
+import Pagination from '../Pagination';
 
 const Panel = (props: PanelProps) => {
+  const [ page, setPage ] = useState<number>(1);
+
+  useEffect(() => {
+    if (props.pagination) {
+      setPage(props.pagination!.currentPage);
+    }
+  }, [props.pagination])
+
+  useEffect(() => {
+    if (props.setParentPage && props.pagination && ( page !== props.pagination!.currentPage)) {
+      props.setParentPage(page);
+    }
+  }, [props.setParentPage, page]);
 
   return (
     <PanelWrapper>
-      {props.children}
+      <div className="panel-title">
+        <h3>{props.title}</h3>
+      </div>
+      <div className="panel-content">
+        {props.message ? (
+          <MessageBox {... props.message} />
+        ) : (
+          null
+        )}
+        {props.children}
+        {props.pagination ? (
+          <Pagination {... { meta: props.pagination, page: page, setPage: setPage } } />
+        ) : ( null )}
+      </div>
     </PanelWrapper>
   );
 };
--- dashboard/src/types/site.ts
+++ dashboard/src/types/site.ts
@@ -1,4 +1,12 @@
+import { PaginatedMeta } from './pagination'
+
 export type PanelProps = {
+  title: string,
+  children: JSX.Element,
+  message?: MessageProps | undefined,
+  pagination?: PaginatedMeta,
+  setParentPage?: Function
+}
 
 export type MessageProps = {
   message: string | undefined,
